cfd154b479975ff2c48b3cf794f661b33b5fee9c
Amit Verma <amit.suraj1990@gmail.com>
2026-02-12 13:20:26 +0530

    Fixed SQLite path for Render deployment

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import os
 import sqlite3
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "library.db")
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 app = Flask(__name__)
 
 def init_db():
-    conn = sqlite3.connect('library.db')
+    conn = get_db_connection()
     conn.execute('''CREATE TABLE IF NOT EXISTS books
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
                   title TEXT,
@@ -16,7 +26,7 @@ init_db()
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect('library.db')
+    conn = get_db_connection()
     books = conn.execute("SELECT * FROM books").fetchall()
     conn.close()
     return render_template('index.html', books=books)
@@ -28,19 +38,18 @@ def add_book():
         author = request.form['author']
         year = request.form['year']
 
-        conn = sqlite3.connect('library.db')
+       conn = get_db_connection()
         conn.execute("INSERT INTO books (title, author, year) VALUES (?, ?, ?)",
                      (title, author, year))
         conn.commit()
         conn.close()
-        return redirect('/')
+        return redirect('/')s
 
     return render_template('add_book.html')
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_book(id):
-    conn = sqlite3.connect('library.db')
-
+    conn = get_db_connection()
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
@@ -58,7 +67,7 @@ def edit_book(id):
 
 @app.route('/delete/<int:id>')
 def delete_book(id):
-    conn = sqlite3.connect('library.db')
+    conn = get_db_connection()
     conn.execute("DELETE FROM books WHERE id=?", (id,))
     conn.commit()
     conn.close()
